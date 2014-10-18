f08e5ff0b2800620947254a96ca76d00fcea32cd
Francesco Marano <francesco.mrn24@gmail.com>
2014-10-19 00:32:27 +0200

    - Added __all__ attribute on every module

--- it/mrnfrancesco/framework/wat/lib/clients.py
+++ it/mrnfrancesco/framework/wat/lib/clients.py
@@ -14,6 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+__all__ = ['Curl', 'CurlMulti', 'CurlShare']
+
 import pycurl
 
 from it.mrnfrancesco.framework.wat import conf
--- it/mrnfrancesco/framework/wat/lib/properties.py
+++ it/mrnfrancesco/framework/wat/lib/properties.py
@@ -14,6 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+__all__ = ['Property', 'Constraint', 'Operation']
+
 import re
 
 from it.mrnfrancesco.framework.wat.lib.exceptions import InvalidPropertyError, EmptyValueError
