619661a38d18037c27557db0159d0be471635dbd
Francesco Marano <francesco.mrn24@gmail.com>
2014-11-06 00:32:04 +0100

    - Removed all graph.py content but checkpreconditions() (Prepared for 'relaxed graphplan' implementation)

--- wat/lib/graph.py
+++ wat/lib/graph.py
@@ -16,13 +16,12 @@
 
 import importlib
 
-from wat.lib.components import WatComponent, iswatcomponent
+from wat.lib.components import WatComponent
 from wat.lib.properties import Property, Constraint
-from wat.lib.search import search
 import wat
 
 
-def checkdependencies(preconditions):
+def checkpreconditions(preconditions):
     resolvable = list()
     for precondition in preconditions:
         # check if property value exists into registry
@@ -48,69 +47,3 @@ def checkdependencies(preconditions):
             resolvable.append(precondition)
     # ok, all preconditions are resolved/resolvable
     return resolvable, True
-
-
-class Node(object):
-    def __init__(self, module_class):
-        if iswatcomponent(module_class):
-            self.module = module_class
-        else:
-            raise  # TODO: raise a proper exception to say "this is not a WAT Module"
-
-        if self.module.dependencies:  # if there is at least a dependency
-            dependencies, resolved_or_resolvable = checkdependencies(self.module.dependencies)
-            if resolved_or_resolvable:
-                if dependencies:
-                    self.nodes = dict((prop, ORNode(prop)) for prop in dependencies)
-            else:
-                raise  # TODO: raise a proper exception to say "there are some unresolvable dependencies"
-
-
-class ORNode(object):
-    def __init__(self, property_name):
-        self.property_name = property_name
-
-        try:
-            self.nodes = [Node(module_class=result) for result in search(prop=property_name)]
-        except:  # TODO: catch and handle the exception raised by search (?)
-            pass
-        if not hasattr(self, 'nodes'):
-            raise  # TODO: raise a proper exception to say "there are unresolvable dependencies"
-
-
-def findpaths(properties):
-    if isinstance(properties, str):
-        properties = list(properties)
-    return [ORNode(prop) for prop in properties]
-
-
-# WatComponent.provide('website.cms.name', 'opencart')
-# WatComponent.provide('website.cms.opencart.admin.directory', 'admin')
-# n = ORNode('website.cms.opencart.version')
-# print n
-
-# from digraphtools import *
-#
-# dag = graph_from_edges([(1, 2), (2, 3), (3, 4), (2, 4), (4, 5), (5, 3)])
-# print dag
-# for node in dfs_topsort_traversal(dag, 1):
-#     print node
-
-requested_props = ["website.cms.opencart.version"]
-to_process_props = requested_props
-involved_props = dict()
-
-for processing_property in to_process_props:
-    watmodules = search(prop=processing_property)
-    for watmodule in watmodules:
-        if watmodule.dependencies:
-            to_process_props.extend([str(dependency) for dependency in watmodule.dependencies])
-    involved_props[processing_property] = watmodules
-
-print "Involved Properties: "
-print involved_props
-
-if set(requested_props).issubset(set([key for key, value in involved_props.items() if value])):
-    print 'ok'
-else:
-    print 'ko'
