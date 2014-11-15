c5524d311064c54b58cd5ffb982573f4d4a3ffbf
Francesco Marano <francesco.mrn24@gmail.com>
2014-11-15 01:44:18 +0100

    - Implemented GetAdminDirByDefault component to check if the admin directory is the default one

--- wat/components/website/cms/opencart/admin/directory/default.py
+++ wat/components/website/cms/opencart/admin/directory/default.py
@@ -13,3 +13,48 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from datetime import date
+from pycurl import URL, WRITEFUNCTION, HTTP_CODE
+from wat import conf
+from wat.lib import clients
+
+from wat.lib.components import WatComponent, info, MetaComponent
+from wat.lib.exceptions import ComponentFailure
+from wat.lib.models import Author
+from wat.lib.properties import Constraint
+
+
+@info(
+    authors=[
+        Author(email="francesco.mrn24@gmail.com", name="Francesco Marano", nickname="mrnfrancesco"),
+    ],
+    released=date(2014, 11, 15),
+    updated=date(2014, 11, 15),
+    version='0.0.1',
+    preconditions=[
+        Constraint("website.cms.name", "opencart", 'eq'),
+    ]
+)
+class GetAdminDirByDefault(WatComponent):
+    __metaclass__ = MetaComponent
+
+    def __init__(self):
+        super(GetAdminDirByDefault, self).__init__()
+
+        from urlparse import urljoin
+        admin_url = urljoin(conf.clients.instance().URL, 'admin/')
+
+        self.curl = clients.Curl()
+        self.curl.setopt(URL, admin_url)
+        self.curl.setopt(WRITEFUNCTION, '/dev/null')
+
+    def run(self):
+        self.curl.perform()
+        http_code = self.curl.getinfo(HTTP_CODE)
+        if http_code is 200:
+            return 'admin/'
+        elif http_code is 404:
+            raise ComponentFailure('Admin directory name is not the default one')
+        else:
+            raise ComponentFailure("Server response HTTP status code was '%d', 200 or 404 expected" % http_code)
