1ea2fcff5b12e936f8f453ae8e9ebe4b9362dd31
Francesco Marano <francesco.mrn24@gmail.com>
2014-11-21 03:36:33 +0100

    - Written a first sketch of GetAdminDirByBruteforce component

--- wat/components/website/cms/opencart/admin/directory/bruteforce.py
+++ wat/components/website/cms/opencart/admin/directory/bruteforce.py
@@ -13,3 +13,54 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from datetime import date
+import logging
+from pycurl import URL, WRITEFUNCTION, HTTP_CODE
+
+from wat import conf
+from wat.lib import clients
+from wat.lib.components import WatComponent, info, MetaComponent
+from wat.lib.exceptions import ComponentFailure
+from wat.lib.models import Author
+from wat.lib.properties import Constraint
+from wat.lib.shortcuts import hierlogger as logger
+
+
+@info(
+    authors=[
+        Author(email="francesco.mrn24@gmail.com", name="Francesco Marano", nickname="mrnfrancesco"),
+    ],
+    released=date(2014, 11, 21),
+    updated=date(2014, 11, 21),
+    version='0.0.1',
+    preconditions=[
+        Constraint("website.cms.name", "opencart", 'eq'),
+    ]
+)
+class GetAdminDirByBruteforce(WatComponent):
+    """This module try to provide the OpenCart admin directory trying some common ones."""
+    __metaclass__ = MetaComponent
+
+    def __init__(self):
+        super(GetAdminDirByBruteforce, self).__init__()
+
+        self.curl = clients.Curl()
+        self.curl.setopt(WRITEFUNCTION, self.save_as_attribute('_'))
+        self.admin_dirs = ['admin/', 'administrator/', 'administration/', 'sysadmin/']
+
+    def run(self):
+        from urlparse import urljoin
+        debug_enabled = logger().isEnabledFor(logging.DEBUG)
+
+        for admin_dir in self.admin_dirs:
+            self.curl.setopt(URL, urljoin(conf.clients.instance().URL, 'admin/'))
+            self.curl.perform()
+            http_code = self.curl.getinfo(HTTP_CODE)
+            if http_code is 200:
+                return admin_dir
+            elif debug_enabled:
+                if http_code is 404:
+                    logger().debug("Admin directory name is not '%s'" % admin_dir)
+                else:
+                    logger().debug("Server response HTTP status code was '%d', 200 or 404 expected" % http_code)
