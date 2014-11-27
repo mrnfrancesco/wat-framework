0090fe133612ab7932a3b768dbde20689a318029
Francesco Marano <francesco.mrn24@gmail.com>
2014-11-27 16:01:17 +0100

    + Recoded all the search module    - Added components(postcondition, preconditions) to search components    - Added properties(containing, not_containing) to search properties    - Changed visibility of utility functions from public to private

--- wat/lib/search.py
+++ wat/lib/search.py
@@ -14,56 +14,30 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+__all__ = {'components', 'properties'}
+
 import importlib
 import inspect
 from pkgutil import walk_packages, os
 
-from wat.lib.components import iswatcomponent, module_from
 import wat
-from wat.lib.exceptions import PropertyDoesNotExist
+from wat.lib.components import iswatcomponent, component_from
+from wat.lib.exceptions import PropertyDoesNotExist, InvalidTypeError
 
 
-def submoduleof(module):
+def __submoduleof(module):
     for submodule_name in module.__modules__:
         yield '.'.join([module.__name__, submodule_name])
 
 
-def componentsin(module):
+def __componentsin(module):
     return filter(
         lambda cls: cls.__module__ == module.__name__ and iswatcomponent(cls),
         [member[1] for member in inspect.getmembers(module, inspect.isclass)]
     )
 
 
-def search(prop=None, preconditions=None):
-    components = list()
-    if prop is not None:
-        try:
-            module = importlib.import_module('.'.join([wat.packages.components, prop]))
-            if hasattr(module, '__modules__') and module.__modules__:
-                for submodule_name in submoduleof(module):
-                    try:
-                        submodule = importlib.import_module(submodule_name)
-                        components.extend([cls for cls in componentsin(submodule)])
-                    except ImportError:
-                        continue
-        except ImportError:
-            raise PropertyDoesNotExist(prop)
-
-    if preconditions is not None:
-        # if we have no components, get them all and then filter by preconditions
-        # otherwise filter them only
-        if not components:
-            components = all_components()
-
-        components = filter(
-            lambda component_class: not set(str(dep) for dep in component_class.dependencies).isdisjoint(preconditions),
-            components
-        )
-    return components
-
-
-def all_components():
+def __components():
     components = list()
     paths = [wat.dirs.components]
     for path in paths:
@@ -71,5 +45,83 @@ def all_components():
             if ispackage:
                 paths.append(package_or_path)
             else:
-                components.extend(componentsin(importlib.import_module(module_from(package_or_path))))
+                components.extend(__componentsin(importlib.import_module(component_from(package_or_path))))
+
     return components
+
+
+def components(postcondition=None, preconditions=None):
+    # no filters means all components
+    if postcondition is None and preconditions is None:
+        return __components()
+    else:
+        if postcondition is not None:
+            if not isinstance(postcondition, str):
+                raise InvalidTypeError(postcondition, (str,))
+        if preconditions is not None:
+            if not isinstance(preconditions, (list, set, tuple)):
+                raise InvalidTypeError(preconditions, (list, set, tuple))
+            elif isinstance(preconditions, (list, tuple)):
+                preconditions = set(preconditions)
+
+    components = list()
+    if postcondition is not None:
+        # take all the components which give the specified postcondition
+        try:
+            module = importlib.import_module('.'.join([wat.packages.components, postcondition]))
+            if hasattr(module, '__modules__') and module.__modules__:
+                for submodule_name in __submoduleof(module):
+                    try:
+                        submodule = importlib.import_module(submodule_name)
+                        components.extend([cls for cls in __componentsin(submodule)])
+                    except ImportError:
+                        continue
+        except ImportError:
+            raise PropertyDoesNotExist(postcondition)
+
+    if preconditions is not None:
+        # if we have no postcondition filter, get all the components
+        # then filter by preconditions, otherwise filter them only
+        if postcondition is None:
+            components = __components()
+
+        components = filter(
+            lambda component: not set(str(dep) for dep in component.preconditions).isdisjoint(preconditions),
+            components
+        )
+    return components
+
+
+def __properties():
+    properties = set()
+    for component in __components():
+        properties.add(str(component.postcondition))
+        for prop in component.preconditions:
+            properties.add(str(prop))
+
+    return properties
+
+
+def properties(containing=None, not_containing=None):
+    if containing is not None:
+        if not isinstance(containing, (list, set, tuple)):
+            raise InvalidTypeError(containing, (list, set, tuple))
+    if not_containing is not None:
+        if not isinstance(not_containing, (list, set, tuple)):
+            raise InvalidTypeError(not_containing, (list, set, tuple))
+
+    properties = __properties()
+
+    if containing is not None:
+        properties = filter(
+            lambda prop: any(search_term in prop for search_term in containing),
+            properties
+        )
+
+    if not_containing is not None:
+        properties = filter(
+            lambda prop: not any(search_term in prop for search_term in containing),
+            properties
+        )
+
+    return properties
