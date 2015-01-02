951cba3679937bf7304075c5d569db361af01293
Francesco Marano <francesco.mrn24@gmail.com>
2015-01-02 23:47:26 +0100

    - Redesigned component to catch opencart admin session (it now takes rigth values)

--- wat/components/website/cms/opencart/admin/session/admin_login.py
+++ wat/components/website/cms/opencart/admin/session/admin_login.py
@@ -1,6 +1,6 @@
 import re
 from datetime import date
-from pycurl import URL, HTTP_CODE, HTTPPOST, EFFECTIVE_URL, FOLLOWLOCATION, WRITEFUNCTION, HEADERFUNCTION
+from pycurl import URL, HTTP_CODE, HTTPPOST, WRITEFUNCTION, HEADERFUNCTION
 
 from wat import conf
 from wat.lib import clients
@@ -16,7 +16,7 @@ from wat.lib.properties import Constraint, Property
     ],
     released=date(2014, 12, 18),
     updated=date(2015, 01, 02),
-    version='0.1.0',
+    version='1.0.0',
     preconditions=[
         Constraint("website.cms.name", "opencart", 'eq'),
         Property("website.cms.opencart.admin.directory"),
@@ -31,17 +31,20 @@ class GetAdminSessionByLogin(WatComponent):
         super(GetAdminSessionByLogin, self).__init__()
         from urlparse import urljoin
 
-        def __get_phpsessionid(header):
-            print header
-            match = re.match("^Set-Cookie: .*PHPSESSID=(?P<phpsessid>[a-z0-9]+);.*$", header)
-            if match:
-                self.phpsessid = match.group('phpsessid')
+        def __get_session(header):
+            if not self.session.has_key('phpsessid'):
+                match = re.match("^Set-Cookie: .*PHPSESSID=(?P<phpsessid>[a-z0-9]{26});.*\r\n", header)
+                if match:
+                    self.session['phpsessid'] = match.group('phpsessid')
+            if not self.session.has_key('token'):
+                match = re.match("^Location: .*token=(?P<token>[0-9a-f]{32})\r\n$", header)
+                if match:
+                    self.session['token'] = match.group('token')
 
-        self.phpsessid = None
         self.curl = clients.Curl()
-        self.curl.setopt(HEADERFUNCTION, __get_phpsessionid)
+        self.session = dict()
+        self.curl.setopt(HEADERFUNCTION, __get_session)
         self.curl.setopt(WRITEFUNCTION, self.save_as_attribute('_'))
-        self.curl.setopt(FOLLOWLOCATION, True)
         url = urljoin(
             conf.clients.instance().URL,
             self.precondition("website.cms.opencart.admin.directory") + 'index.php?route=common/login'
@@ -56,19 +59,13 @@ class GetAdminSessionByLogin(WatComponent):
     def run(self):
         self.curl.perform()
         http_code = self.curl.getinfo(HTTP_CODE)
-        if http_code == 200:
-            match = re.match(
-                r'.+/index\.php\?route=common/home&token=(?P<token>[0-9a-f]{32})$',
-                self.curl.getinfo(EFFECTIVE_URL)
-            )
-            if match is not None and self.phpsessid is not None:
-                return {
-                    'phpsessid': self.phpsessid,
-                    'token': match.group('token'),
-                }
-            elif match is None:
-                raise ComponentFailure("Unable to retrieve session token")
+        if http_code == 302:
+            if self.session.has_key('phpsessid'):
+                if self.session.has_key('token'):
+                    return self.session
+                else:
+                    raise ComponentFailure("Unable to retrieve session token")
             else:
-                raise ComponentFailure("Unable to retrieve PHPSESSID cookie")
+                raise ComponentFailure("Unable to retrieve PHP session ID")
         else:
             raise ComponentFailure("Server response HTTP status code was %d, 200 expected" % http_code)
