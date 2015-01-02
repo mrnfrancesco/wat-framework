9aff7eb5c0cdfff4b29172a63544d9904a9f05b6
Francesco Marano <francesco.mrn24@gmail.com>
2015-01-02 20:21:03 +0100

    - Moved component for property website.cms.opencart.admin.session.token into website.cms.opencart.admin.session - Added PHPSESSID as result (near to session token) to succesfull do authenticated requests

---
+++ wat/components/website/cms/opencart/admin/session/__init__.py
@@ -0,0 +1,1 @@
+__provides__ = {'phpsessid', 'token'}
--- wat/components/website/cms/opencart/admin/session/admin_login.py
+++ wat/components/website/cms/opencart/admin/session/admin_login.py
@@ -1,6 +1,6 @@
 import re
 from datetime import date
-from pycurl import URL, HTTP_CODE, HTTPPOST, EFFECTIVE_URL, FOLLOWLOCATION, WRITEFUNCTION
+from pycurl import URL, HTTP_CODE, HTTPPOST, EFFECTIVE_URL, FOLLOWLOCATION, WRITEFUNCTION, HEADERFUNCTION
 
 from wat import conf
 from wat.lib import clients
@@ -15,23 +15,31 @@ from wat.lib.properties import Constraint, Property
         Author(email="francesco.mrn24@gmail.com", name="Francesco Marano", nickname="mrnfrancesco"),
     ],
     released=date(2014, 12, 18),
-    updated=date(2014, 12, 18),
-    version='0.0.1',
+    updated=date(2015, 01, 02),
+    version='0.1.0',
     preconditions=[
         Constraint("website.cms.name", "opencart", 'eq'),
         Property("website.cms.opencart.admin.directory"),
         Property("website.cms.opencart.admin.credentials"),
     ]
 )
-class GetAdminTokenFromUrl(WatComponent):
-    """Get the OpenCart admin token to make authenticated requests."""
+class GetAdminSessionByLogin(WatComponent):
+    """Get the OpenCart admin session id and session token to make authenticated requests."""
     __metaclass__ = MetaComponent
 
     def __init__(self):
-        super(GetAdminTokenFromUrl, self).__init__()
+        super(GetAdminSessionByLogin, self).__init__()
         from urlparse import urljoin
 
+        def __get_phpsessionid(header):
+            print header
+            match = re.match("^Set-Cookie: .*PHPSESSID=(?P<phpsessid>[a-z0-9]+);.*$", header)
+            if match:
+                self.phpsessid = match.group('phpsessid')
+
+        self.phpsessid = None
         self.curl = clients.Curl()
+        self.curl.setopt(HEADERFUNCTION, __get_phpsessionid)
         self.curl.setopt(WRITEFUNCTION, self.save_as_attribute('_'))
         self.curl.setopt(FOLLOWLOCATION, True)
         url = urljoin(
@@ -53,9 +61,14 @@ class GetAdminTokenFromUrl(WatComponent):
                 r'.+/index\.php\?route=common/home&token=(?P<token>[0-9a-f]{32})$',
                 self.curl.getinfo(EFFECTIVE_URL)
             )
-            if match is not None:
-                return match.group('token')
+            if match is not None and self.phpsessid is not None:
+                return {
+                    'phpsessid': self.phpsessid,
+                    'token': match.group('token'),
+                }
+            elif match is None:
+                raise ComponentFailure("Unable to retrieve session token")
             else:
-                raise ComponentFailure("Unable to retrieve token from URL")
+                raise ComponentFailure("Unable to retrieve PHPSESSID cookie")
         else:
             raise ComponentFailure("Server response HTTP status code was %d, 200 expected" % http_code)
